af8061fd363443e8142d1a23ebb57f3e9c76b449
dima666666 <125134368+dima666666@users.noreply.github.com>
2023-02-18 17:59:05 +0300

    Add files via upload

---
+++ Untitled17.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#задание 1
+spring=(3,4,5)
+winter=(12,1,2)
+summer=(6,7,8)
+fall=(9,10,11)
+a=int(input("введиномер месяца:"))
+if (a in winter):
+  print("сейчас весна")
+elif (a in spring):
+   print("сейчас лето ")
+elif (a in summer):
+   print("сейчас осень")
+elif (a in fall):
+   print("сейчас зима")
+else:
+    print("неверный ответ")
+
+
+# In[10]:
+
+
+#задание №2
+a=int(input())
+if a<=0:
+   print(false)
+else:
+   print(true)
+
+
+# In[ ]:
+
+
+
+
